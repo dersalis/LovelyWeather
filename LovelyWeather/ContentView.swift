cbdd2a4bde89e7094bdbe6ae64ae6644ccbe6340
Damian <hello@damianruta.pl>
2022-09-20 21:00:40 +0200

    Initial Commit

---
+++ LovelyWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LovelyWeather
+//
+//  Created by Damian Ruta on 20/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LovelyWeather/LovelyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovelyWeatherApp.swift
+//  LovelyWeather
+//
+//  Created by Damian Ruta on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LovelyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
