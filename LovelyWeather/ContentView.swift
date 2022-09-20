9019d5405a4b967ef99dbbcb6ad31f9e9b5333e4
Damian <hello@damianruta.pl>
2022-09-20 22:45:28 +0200

    Location, Map

--- LovelyWeather/ContentView.swift
+++ LovelyWeather/ContentView.swift
@@ -6,20 +6,65 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-            Text("Witaj, świecie")
-        }
-        
+    @StateObject private var viewModel = ContentViewModel()
+    
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.90360611831819, longitude: 16.566844805651744), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
+        var body: some View {
+        Map(coordinateRegion: $region, showsUserLocation: true)
+                .ignoresSafeArea()
+                .onAppear{
+                    viewModel.checkIfLocationServicesIsEnabled()
+                }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager?
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            //checkLocationAuthorization()
+        } else {
+            print("Turn on location!")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        guard let locationManager = locationManager else {
+            return
+        }
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted likely due to parental controls.")
+        case .denied:
+            print("You have danied this app location permmission. Go into settings to change it")
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
     }
 }
